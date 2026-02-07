b1f1c0daab394cf233990dd9734ad206bfd11a3b
Nayraiyt <naomimorganc@gmail.com>
2026-02-07 11:34:49 -0500

    Tweeked rotation positions to be more natural

--- Main.java
+++ Main.java
@@ -50,28 +50,33 @@ public class Main extends Application {
 
         Rectangle attackLine = new Rectangle(450,10, Color.PINK);
 
-        Circle p1 = new Circle(15, Color.BLUE);
-        Circle p2 = new Circle (15, Color.RED);
-        Circle p3 = new Circle(15, Color.RED);
-        Circle p4 = new Circle(15, Color.RED);
-        Circle p5 = new Circle(15, Color.RED);
-        Circle p6 = new Circle(15, Color.RED);
-        attackLine.relocate(0,150);
-        int starting [][] = {{385,285},{385,50},{210,50},{50,50},{50,285},{210,285}};
+        Circle p1 = new Circle(15,Color.BLUE);
+        Circle p2 = new Circle(15,Color.RED);
+        Circle p3 = new Circle(15,Color.RED);
+        Circle p4 = new Circle(15,Color.RED);
+        Circle p5 = new Circle(15,Color.RED);
+        Circle p6 = new Circle(15,Color.RED);
 
+        int starting [][] = {{385,285},{385,50},{210,50},{50,50},{50,285},{210,285}};
+        Circle positions[] = {p1,p2,p3,p4,p5,p6};
+        for(int i = 0; i<6; i++){
+            int j = 0;
+            positions[i].setLayoutX(starting[i][j]);
+            positions[i].setLayoutY(starting[i][j+1]);
+        }
+        attackLine.relocate(0,150);
         //five one rotations, justified to setter postion.
         int five_one_one [][] = {{400,100},{385,50},{75,100},{50,275},{210,300},{385,275}};
         int five_one_two [][] = {{400,50},{45,125},{30,100},{50,275},{210,300},{385,275}};
-        int five_one_three [][] = {{370,50},{25,150},{50,275},{210,300},{385,275},{410,100}};
-        int five_one_four [][] = {{15,20},{50,275},{210,300},{385,275},{45,125},{20,50}};
-        int five_one_five [][] = {{160,60},{210,300},{385,275},{415,135},{50,275},{20,40}};
-        int five_one_six [][] = {{210,50},{385,275},{400,100},{300,30},{50,275},{220,300}};
-        int five_one[][][] = {five_one_one,five_one_two,five_one_three,
-                             five_one_four,five_one_five,five_one_six};
-        Circle positions[] = {p1,p2,p3,p4,p5,p6};
-
-        AtomicInteger rotationNum = new AtomicInteger(0);
-
+        int five_one_three [][] = {{370,50},{25,150},{50,275},{230,300},{385,275},{410,100}};
+        int five_one_four [][] = {{15,20},{50,275},{210,300},{385,235},{45,125},{20,50}};
+        int five_one_five [][] = {{160,60},{210,300},{385,275},{415,135},{50,250},{20,40}};
+        int five_one_six [][] = {{210,50},{385,300},{400,175},{300,30},{65,250},{220,310}};
+        int five_one[][][] = {five_one_one,five_one_six,five_one_five,
+                             five_one_four,five_one_three,five_one_two};
+                             
+        int setterPos = -1;
+        AtomicInteger rotationNum = new AtomicInteger(setterPos);
 
         rotate.setOnAction(event -> {
             rotationNum.set((rotationNum.get() + 1) % 6);
@@ -91,7 +96,6 @@ public class Main extends Application {
         Scene sc = new Scene(screan, 450, 600);
         stage.setScene(sc);
         stage.show();
-
     }
 
     public static void main(String[] args) {
