f1d21991d9bbf5d3fd2625e68f690ec3153a074f
Nayraiyt <naomimorganc@gmail.com>
2026-02-07 11:01:07 -0500

    Added 51 positions 4-6

--- Main.java
+++ Main.java
@@ -52,29 +52,23 @@ public class Main extends Application {
         Circle p4 = new Circle(15, Color.RED);
         Circle p5 = new Circle(15, Color.RED);
         Circle p6 = new Circle(15, Color.RED);
-        //starting player locations
         attackLine.relocate(0,150);
-        int starting[][] = {{385,285},{385,50},{210,50},{50,50},{50,285},{210,285}};
-        int five_one_one[][] = {{400,100},{385,50},{75,100},{50,275},{210,300},{385,275}};
-        int five_one_two[][] = {{400,50},{45,125},{30,100},{50,275},{210,300},{385,275}};
-        int five_one_three[][] = {{370,50},{25,150},{50,275},{210,300},{385,275},{410,100}};
+        int starting [][] = {{385,285},{385,50},{210,50},{50,50},{50,285},{210,285}};
+
+        //five one rotations, justified to setter postion.
+        int five_one_one [][] = {{400,100},{385,50},{75,100},{50,275},{210,300},{385,275}};
+        int five_one_two [][] = {{400,50},{45,125},{30,100},{50,275},{210,300},{385,275}};
+        int five_one_three [][] = {{370,50},{25,150},{50,275},{210,300},{385,275},{410,100}};
+        int five_one_four [][] = {{15,20},{50,275},{210,300},{385,275},{45,125},{20,50}};
+        int five_one_five [][] = {{160,60},{210,300},{385,275},{415,135},{50,275},{20,40}};
+        int five_one_six [][] = {{210,50},{385,275},{400,100},{300,30},{50,275},{220,300}};
         Circle positions[] = {p1,p2,p3,p4,p5,p6};
 
         for(int i = 0; i<6; i++){
             int j = 0;
-            positions[i].setLayoutX(five_one_three[i][j]);
-            positions[i].setLayoutY(five_one_three[i][j+1]);
+            positions[i].setLayoutX(five_one_six[i][j]);
+            positions[i].setLayoutY(five_one_six[i][j+1]);
         }
-
-        /*
-        p1.setLayoutX(385);
-        p1.setLayoutY(285);
-        p2.relocate(385, 50);
-        p3.relocate(210,50);
-        p4.relocate(50,50);
-        p5.relocate(50,285);
-        p6.relocate(210,285);
-        */
 
         court.getChildren().addAll(attackLine,p2,p3,p4,p5,p6,p1);
 
